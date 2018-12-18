cb8d0ae003816f97c654a6d4db4b004fde0659b8
AlbosHajdari <albos.hajdari@yahoo.com>
2018-12-18 11:25:14 +0100

    CommitNrFIVE

--- app/src/main/java/com/example/lenovo/weekthree_daytwo/MainActivity.java
+++ app/src/main/java/com/example/lenovo/weekthree_daytwo/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         System.out.println("commitNr1");
+        //b
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
